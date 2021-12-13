4b3b27e15beb6503c33ddd66c487a14c112c0f85
Yuning Zhang <neuromancer43@gmail.com>
2021-12-13 21:22:05 +0100

    add `SpinEnsemble` and interfaces

--- src/SpinEnsembles.jl
+++ src/SpinEnsembles.jl
@@ -11,40 +11,114 @@ using Statistics
 import ProgressMeter: @showprogress
 
 # functions
-export dipolarcoef, dipolarcoefs, dipolarlinewidth, randcoefs,
-       fid, averagefid, rabi, betasampling, decaytime,
+export dipolarcoef, dipolarcoefs, dipolarlinewidth, randlocs, randcoefs,
+       fid, averagefid, rabi, betasampling, dephasingtime,
        visualcoupling, visualeffectivebeta, visualensemble, visualfid
 
 # constant
 export fid_plot_options
 
 
-include("RandLoctions.jl")
-include("Visualization.jl")
+include("randloctions.jl")
+include("visualization.jl")
+include("dynamics.jl")
 
 
 """
-    SpinEnsemble
- 
+    SpinEnsemble(n, dim, z0, r, R)
+    SpinEnsemble(n, dim, z0, bound)
+
+# Arguments:
+- `n::Int64`: number of spins in the ensemble 
+- `dim::Int64`: dimension of the ensemble 
+- `z0::Vector{Float64}`: direction of background field 
+- `r::Float64`: minimum radius of distribution area 
+- `R::Float64`: maximum radius of distribution area
+- `rho::Float64`: density of spins
 """
 struct SpinEnsemble
-    n::Int  
-    dim::Int 
-    h::Real
-    z0::AbstractVector{<:Real}
+    n::Int
+    dim::Int
+    z0::Vector{Float64}
     
-    r::Real
-    R::Real
-    a:: Real
-    rho::Real
+    r::Float64
+    R::Float64
+    rho::Float64
+    shape::Symbol
+
+    # a:: Float64
     # f::Real
+    # h::Real
+    locs::Matrix{Float64}
+    D::Vector{Float64}
 
-    locs::AbstractMatrix{<:Real}
-    D::AbstractVector{<:Real}
+    function SpinEnsemble(n::Int,dim::Int,z0::AbstractVector{<:Real},
+    r::Real, R::Real, shape=:spherical)
+        @assert shape in (:spherical, :cubic)
 
-    # islatticed::Bool
-    # isdilute::Bool
+        if shape==:spherical
+            rho=(n/(2*(R-r)),n/(π*(R^2-r^2)),n/(4π/3*(R^3-r^3)))[dim]
+        elseif shape==:cubic
+            rho=(n/(2*(R-r)),n/(4*(R^2-r^2)),n/(8*(R^3-r^3)))[dim]
+        end
+        
+        return new(n,dim,normalize(z0),Float64(r),Float64(R),rho,shape)
+    end
+
+    function SpinEnsemble(n::Int,dim::Int,z0::AbstractVector{<:Real},
+        bound::Tuple{Real,Real}, shape=:spherical)
+        return SpinEnsemble(n, dim, z0, bound[0], bound[1], shape)
+    end
 end
+
+"""
+    randlocs(n, dim, R)
+    randlocs(n, dim, bound; method)
+
+Randomly distribute a spin bath, generate the dipolar coupling strength between the centered spin and bath, 
+totally `n` spins are uniformly distributed in a `dim` dimensional cubic space with lenght `a`
+
+# Arguments
+- `n`: number of spins in ensemble
+- `dim`: dimension
+- `R`: scale of ensemble
+- `bound::Tuple{Real, Real}`: tuple, indicate bounds of sampling range 
+
+# Options
+- `method`: constant, `:spherical` for spherical coordinates or `:cubic` for Cartesian coordinates
+"""
+function randlocs(n::Int, dim::Int, bound::Tuple{Real,Real}; method=:cubic)
+    @assert dim>0 && dim<4
+    @assert method in (:spherical, :cubic)
+    a,b=bound
+    @assert a>=0 && b>=0
+    a,b= a<b ? (a,b) : (b,a)
+
+    if method==:cubic
+        locs=randcartesianlocs(n,a,b, dim=dim)
+    else
+        if dim==3
+            locs=randsphericallocs(n,a,b)
+        elseif dim==2
+            locs=randpolarlocs(n,a,b)
+        else
+            locs=randcartesianlocs(n,a,b, dim=1)
+        end
+    end
+    return locs
+end
+
+randlocs(n::Int,dim::Int,R=1::Real; method=:cubic)=randlocs(n,dim,(0,R);method=method)
+
+function randlocs(spins::SpinEnsemble)
+    return randlocs(spins.n, spins.dim, (spins.r,spins.R); method=spins.shape)
+end
+
+function randlocs!(spins::SpinEnsemble)
+    spins.locs=randlocs(spins)
+end
+
+
 
 @doc raw"""
     dipolarcoef(r, z0)
@@ -76,7 +150,6 @@ function dipolarcoef(r::AbstractVector{<:Real},z0::AbstractVector{<:Real})
     return D
 end
 
-
 """
     dipolarcoefs(locs, z0)
 
@@ -88,9 +161,8 @@ Get a list of dipolar coupling strength between the centered spin and bath
 """
 function dipolarcoefs(locs::Matrix{<:Real},z0=[0,0,1.0]::AbstractVector{<:Real})
     normalize!(z0)
-    map(x->dipolarcoef(x,z0), eachrow(locs))
+    return map(x->dipolarcoef(x,z0), eachrow(locs))
 end
-
 
 """
     randcoefs(n, dim, R)
@@ -109,27 +181,17 @@ totally `n` spins are uniformly distributed in a `dim` dimensional cubic space w
 - `method`: constant, `:spherical` for spherical coordinates or `:cubic` for Cartesian coordinates
 """
 function randcoefs(n::Int, dim::Int, bound::Tuple{Real,Real}; method=:cubic)
-    @assert dim>0 && dim<4
-    @assert method in (:spherical, :cubic)
-    a,b=bound
-    @assert a>=0 && b>=0
-    a,b= a<b ? (a,b) : (b,a)
-
-    if method==:cubic
-        locs=randcartesianlocs(n,a,b, dim=dim)
-    else
-        if dim==3
-            locs=randsphericallocs(n,a,b)
-        elseif dim==2
-            locs=randpolarlocs(n,a,b)
-        else
-            locs=randcartesianlocs(n,a,b, dim=1)
-        end
-    end
-    dipolarcoefs(locs)
+    return dipolarcoefs(randlocs(n, dim, bound; method=method))
 end
 
 randcoefs(n::Int,dim::Int,R=1::Real; method=:cubic)=randcoefs(n,dim,(0,R);method=method)
+
+randcoefs(spins::SpinEnsemble)=dipolar(randlocs(spins.n, spins.dim, (spins.r,spins.R); method=spins.shape))
+
+
+function randlocs!(spins::SpinEnsemble)
+    spins.locs=randcoefs(spins)
+end
 
 @doc raw"""
     betasampling(D)
@@ -162,62 +224,6 @@ b=\sqrt{\sum_j D_j^2}
 """
 dipolarlinewidth(D::Vector{<:Real})=sqrt(mapreduce(abs2,+,D))
 
-@doc raw"""
-    fid(t, D)
-
-Given a set of dipolar coupling constant, 
-calculate et the free induction decay (FID) value at given time `t`, or the FID decay curve for a time array `t`
-
-```math
-f(t)=\frac{1}{2}\prod_j \cos(D_jt)
-```
-# Arguments
-- `t`: time, float or array of float
-- `D`: coupling strength of dipolar interactions, a array of floats
-"""
-fid(t::Real,D::Vector{<:Real})=mapreduce(cos,*,D*t)/2;
-fid(t::AbstractVector{<:Real},D::Vector{<:Real})=map(x->fid(x,D),t);
-
-@doc raw"""
-    fid(t, D, h; N)
-
-Simulate FID signal with given transverse magnetic field `h`, using Monte-Carlo sampling
-
-```math
-f_p(t)=\frac{1}{2}[\cos^2(\omega_p t)+\sin^2(\omega_p t) (n_x^2-n_z^2)]
-```
-
-```math
-\bar{f}(t)=\frac{1}{N}\sum_{p=1}^N f_p(t)
-```
-# Arguments
-- `t`: discrete array marking time 
-- `D`: a set of the coupling strengths
-- `h`: strength of transverse field 
-
-# Options
-- `N`: number of Monte-Carlo sampling
-- `geterr::Bool`: wetehr to return the error of the monte-sampling 
-"""
-function fid(t::AbstractVector{<:Real},D::Vector{<:Real},h::Real; N=100::Int, geterr=:false)
-    n=length(D)
-    f_sum=zeros(length(t)) # sum
-    f_var=copy(f_sum) # square sum
-    for i in 1:N
-        β_p=sum(rand([1,-1],n).*D) 
-        ω_p=sqrt(h^2+β_p^2)/2
-        cos2_p=cos.(ω_p*t).^2
-        f_p=(cos2_p+(cos2_p.-1)*(β_p^2-h^2)/(h^2+β_p^2))/2
-        f_sum+=f_p
-        f_var+= i>1 ? (i*f_p-f_sum).^2/(i*(i-1)) : f_var
-    end
-    if geterr
-        return (f_sum/N,f_var/(N-1))
-    else
-        return f_sum/N
-    end
-end
-
 
 @doc raw"""
     averagefid(t, M, sampling_D; [options]...)
@@ -245,7 +251,7 @@ function averagefid(t::AbstractVector{<:Real}, M::Int, sampling_D::Function)
 end
 
 @doc raw"""
-    decaytime(D, len=500; scale=1.5)
+    dephasingtime(D, len=500; scale=1.5)
 
 The FID is Fourier transform of the noise spectrum. 
 For a Gaussian noise with linewidth `b`, it's characteristic function is 
@@ -264,107 +270,15 @@ T_2=\frac{\pi}{b}
 # Options 
 - `scale`: scale factor to extend the T_2 
 """
-function decaytime(D::Vector{<:Real}, len=500::Int; scale=1.5::Real)
+function dephasingtime(D::Vector{<:Real}, len=500::Int; scale=1.5::Real)
     b=dipolarlinewidth(D)
     t=π/b*scale 
     dt=t/len
     return 0:dt:t 
 end
 
-@doc raw"""
-    rabi(t, D, h; N, options...)
+dephasingtime(D::Vector{<:Real})=π/dipolarlinewidth(D)
 
-Get a random sampling of, under given transverse magnetic field
-
-```math
-\begin{aligned}
-z_p(t)&=+\frac{1}{2} \left[n_z^2+n_x^2 \cos (\Omega_p t)\right]\\
-y_p(t)&=-\frac{1}{2} n_x \sin (\Omega_p t)\\
-x_p(t)&=+\frac{1}{2} \left[n_x n_z-n_x n_z \cos (\Omega_p t)\right]
-\end{aligned}
-```
-
-```math
-G(t)=\frac{1}{N}\sum_{p=1}^N g_p(t),\; g=x,y,z
-```
-
-# Arguments
-- `t`: discrete array marking time 
-- `D`: a set of the coupling strengths
-- `h`: strength of transverse field 
-
-# Options
-- `N`: size of Monte-Carlo sampling, default at 100
-- `axis::Int`: , 1,2,3, representing x,y,z axis, set to 3 by default 
-- `returnerr::Bool`: wether to return the variance in sampling
-"""
-function rabi(t::AbstractVector{<:Real}, D::Vector{<:Real}, h::Real; N=100::Int, axis=3::Int, returnerr=:false)
-    @assert axis in (1,2,3)
-    n=length(D)
-    f_sum=zeros(length(t)) # sum
-    f_var=copy(f_sum) # square sum
-    f_sampling=(_rabix,_rabiy,_rabiz)[axis]
-
-    for i in 1:N
-        β_p=sum(rand([1,-1],n).*D) 
-        f_p=f_sampling(t,β_p,h)
-        f_sum+=f_p
-        f_var+= i>1 ? (i*f_p-f_sum).^2/(i*(i-1)) : f_var
-    end
-    if returnerr
-        return (f_sum/N,f_var/(N-1))
-    else
-        return f_sum/N
-    end
-end
-
-function _rabiz(t::AbstractVector{<:Real},β::Real,h::Real)
-    ω=sqrt(h^2+β^2)/2
-    cos2=cos.(ω*t).^2
-    return (cos2-(cos2.-1)*(β^2-h^2)/(h^2+β^2))/2
-end
-
-function _rabiy(t::AbstractVector{<:Real},β::Real,h::Real)
-    Ω=sqrt(h^2+β^2)
-    return -sin.(Ω*t)/2*h/Ω
-end
-
-function _rabix(t::AbstractVector{<:Real},β::Real,h::Real)
-    ω=sqrt(h^2+β^2)/2
-    sin2=sin.(ω*t).^2
-    return sin2*(β*h)/(h^2+β^2)
-end
-
-
-@doc raw"""
-    rabi(t, h, b; [axis])
-
-Get analytical solution of Rabi oscillation under Gaussian noise with linewidth `b`
-
-```math
-M_z(t)=A \cos(h \,t+\varphi), \quad M_y(t)=-A \sin(h \,t+\varphi)
-```
-```math
-A=\frac{1}{2}\left(1+b^4t^2/h^2\right)^{1/4},\quad \varphi=\frac{1}{2}\arctan(b^2 t/h)
-```
-
-# Arguments
-- `t`: discrete array marking time 
-- `D`: a set of the coupling strengths
-- `h`: strength of transverse field 
-
-# Options
-- `axis::Int`: , 1,2,3, representing x,y,z axis, set to 3 by default 
-""" 
-function rabi(t::AbstractArray, h::Real, b::Real; axis=3)
-    @assert axis in (1,2,3)
-    A=(1.0.+b^4*t.^2/h^2).^(-1/4)/2
-    φ=atan.(b^2*t/h)/2
-    if axis==3
-        return A.*cos.(h*t+φ)
-    elseif axis==2
-        return -A.*sin.(h*t+φ)
-    end
-end
+dephasingtime(spins::SpinEnsemble)=dephasingtime(spins.D)
 
 end
