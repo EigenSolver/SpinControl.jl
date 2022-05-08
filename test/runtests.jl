6083b46464f03d9ad7930e89ae7c8e865d1f809b
Yuning <neuromancer43@gmail.com>
2022-05-08 14:12:51 +0200

    update version number

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,11 @@ using Test
 using SpinControl
 using LinearAlgebra
 
-# include("test_locations.jl")
-# include("test_datatypes.jl")
-# include("test_dynamics.jl")
-# include("test_driving.jl")
-# include("test_rotations.jl")
-# include("test_fidelity.jl")
-# include("test_pulse.jl")
+include("test_locations.jl")
+include("test_datatypes.jl")
+include("test_dynamics.jl")
+include("test_driving.jl")
+include("test_rotations.jl")
+include("test_fidelity.jl")
+include("test_pulse.jl")
 include("test_sequence.jl")
