e5d48e5aaa85b283cc0300e874eec9d8689e9cbb
Yuning <neuromancer43@gmail.com>
2022-09-22 13:21:53 +0200

    fix coef

--- src/stochastic/stochasticprocess.jl
+++ src/stochastic/stochasticprocess.jl
@@ -1,12 +1,16 @@
 using DiffEqNoiseProcess
 
-function OrnsteinUhlenbeckNoise(n::Int, dt::Real, params::Union{Tuple,AbstractVector{<:Real}})::NoiseProcess
-    θ, μ, σ = params
-    OU=OrnsteinUhlenbeckProcess(θ,μ,σ,0.0,0.0)
+function OrnsteinUhlenbeckNoise(n::Int, dt::Real, params::Union{Tuple,AbstractVector{<:Real}}, X₀::Real = 0)::NoiseProcess
+    θ, μ, σ, = params
+    if X₀ == -1
+        X₀ = σ*randn()
+    end
+    OU=OrnsteinUhlenbeckProcess!(θ,μ,σ,0.0, convert(Float64, X₀))
     OU.dt=dt
-    calculate_step!(OU, dt, nothing, nothing)
+    u = nothing; p= nothing;
+    calculate_step!(OU, dt, u, p)
     for i in 1:n
-        accept_step!(OU,dt, nothing, nothing)
+        accept_step!(OU,dt, u, p)
     end
     return OU
 end
@@ -23,7 +27,7 @@ end
 function fid(noise::NoiseProcess)
     dt=noise.dt 
     phi=cumsum(noise.u)*dt
-    return cos.(2*phi)
+    return cos.(phi)
 end
 
 function rabi(noise::NoiseProcess, h::Union{<:Real,AbstractVector{<:Real}})
